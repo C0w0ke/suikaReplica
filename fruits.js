504690838b4059069bc1f372529bc2b2339f76cc
C0w0ke <hieuvip129@gmail.com>
2024-09-20 23:12:33 +0700

    Update

--- fruits.js
+++ fruits.js
@@ -3,56 +3,56 @@ export const fruitsData = [
         level: 0,
         size: 10,
         score: 5,
-        fruit: "/assets/sprite/strawberry.png"
+        fruit: "./assets/sprite/strawberry.png"
     },
 
     {
         level: 1,
         size: 15,
         score: 10,
-        fruit: "/assets/sprite/grape.png"
+        fruit: "./assets/sprite/grape.png"
     },
 
     {
         level: 2,
         size: 20,
         score: 15,
-        fruit: "/assets/sprite/gyool.png"
+        fruit: "./assets/sprite/gyool.png"
     },
     
     {
         level: 3,
         size: 25,
         score: 20,
-        fruit: "/assets/sprite/orange.png"
+        fruit: "./assets/sprite/orange.png"
     },
 
     {
         level: 4,
         size: 30,
         score: 25,
-        fruit: "/assets/sprite/apple.png"
+        fruit: "./assets/sprite/apple.png"
     },
 
     {
         level: 5,
         size: 35,
         score: 30,
-        fruit: "/assets/sprite/pear.png"
+        fruit: "./assets/sprite/pear.png"
     },
 
     {
         level: 6,
         size: 40,
         score: 35,
-        fruit: "/assets/sprite/melon.png"
+        fruit: "./assets/sprite/melon.png"
     },
 
     {
         level: 7,
         size: 45,
         score: 40,
-        fruit: "/assets/sprite/watermelon.png"
+        fruit: "./assets/sprite/watermelon.png"
     },
 ];
 
